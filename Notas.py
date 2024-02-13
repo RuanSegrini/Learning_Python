4e9de19b15c9bceb26630a69fa385295cd978d4a
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2024-02-13 16:04:14 -0300

    Add files via upload

---
+++ Notas.py
@@ -0,0 +1,14 @@
+nota1: float
+nota2: float
+notaFinal: float
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+
+notaFinal = nota1 + nota2
+
+print(f"NOTA FINAL = {notaFinal:.2f}")
+
+if notaFinal < 60:
+    print(f"REPROVADO")
+    
