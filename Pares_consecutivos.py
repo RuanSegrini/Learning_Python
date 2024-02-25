39b3137a04ca178fc16399a47c07979c017adc9c
Ruan Carolino Segrini <102704092+RuanSegrini@users.noreply.github.com>
2024-02-25 16:49:16 -0300

    Add files via upload

---
+++ Pares_consecutivos.py
@@ -0,0 +1,17 @@
+x:int
+resto: int
+soma: int
+
+x = int(input("Digite um numero inteiro: "))
+resto = x % 2
+
+while x != 0:
+    if resto != 0:
+    
+        soma = (x + 1) + (x + 3) + (x + 5) + (x + 7) + (x + 9)
+        print(f"SOMA = {soma}")
+    else:
+        soma = x + (x + 2) + (x + 4) + (x + 6) + (x + 8)
+        print(f"SOMA = {soma}")
+    x = int(input("Digite um numero inteiro: "))
+    resto = x % 2
